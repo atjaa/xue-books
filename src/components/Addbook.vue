004998ddce4f77e280515c07e2cbbc1a142cafec
lijinzhuang <lijinzhuang@yunzongnet.com>
2018-07-12 14:00:46 +0800

    数据检查字数限制调整

--- src/components/Addbook.vue
+++ src/components/Addbook.vue
@@ -121,7 +121,7 @@ export default {
         ],
         bookname: [
           {required: true, message: '请输入图书名称', trigger: 'blur'},
-          {min: 3, max: 15, message: '长度在 3 到 30 个字符', trigger: 'blur'}
+          {min: 1, max: 15, message: '长度在 1 到 30 个字符', trigger: 'blur'}
         ],
         introduction: [
           {required: true, message: '请输入图书简介', trigger: 'blur'},
